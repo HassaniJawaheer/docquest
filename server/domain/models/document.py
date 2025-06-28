6c828c04f6b4873af917b37607f7d927869fbf52
Jawaheer Hassani <jawaheer.hassani@synchrotron-soleil.fr>
2025-06-28 11:49:47 +0200

    Mise à jour de document.py et de requirements.txt (ajout de redis)

--- server/domain/models/document.py
+++ server/domain/models/document.py
@@ -1,11 +1,8 @@
-from typing import List
 from pydantic import BaseModel
-from .chunk import Chunk
 
 class Document (BaseModel):
     """
     Represents a document
     """
     name: str
-    text: str
-    chunks: List[Chunk]
+    content: str
